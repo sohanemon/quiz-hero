6fb4bbfcd8b97648c57c107d9ad1076835fe8b9a
sohanemon <m.sohanemon@gmail.com>
2022-10-12 21:13:38 +0600

    correct answer showed

--- src/components/quiz-page.jsx
+++ src/components/quiz-page.jsx
@@ -106,13 +106,46 @@ const Questions = ({ question, options, correctAnswer, setSummary }) => {
       });
     }
   };
+  const showCorrectAnswer = () => {
+    toast(correctAnswer, {
+      icon: "✅",
+      autoClose: 2000,
+      hideProgressBar: true,
+      style: { width: "max-content", textAlign: "center" },
+      closeButton: false,
+    });
+  };
 
   return (
     <div className='px-4 py-1 max-w-3xl mx-auto'>
-      <p
-        className='text-lg tracking-wide font-semibold pb-1'
-        dangerouslySetInnerHTML={createMarkup()}
-      />
+      <div className='flex items-center justify-between'>
+        <p
+          className='text-lg tracking-wide font-semibold pb-1'
+          dangerouslySetInnerHTML={createMarkup()}
+        />
+        <svg
+          className='cursor-pointer w-6 h-6'
+          // eye
+          onClick={showCorrectAnswer}
+          xmlns='http://www.w3.org/2000/svg'
+          fill='none'
+          viewBox='0 0 24 24'
+          stroke-width='1.5'
+          stroke='currentColor'
+        >
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            d='M2.036 12.322a1.012 1.012 0 010-.639C3.423 7.51 7.36 4.5 12 4.5c4.638 0 8.573 3.007 9.963 7.178.07.207.07.431 0 .639C20.577 16.49 16.64 19.5 12 19.5c-4.638 0-8.573-3.007-9.963-7.178z'
+          />
+          <path
+            stroke-linecap='round'
+            stroke-linejoin='round'
+            d='M15 12a3 3 0 11-6 0 3 3 0 016 0z'
+          />
+        </svg>
+      </div>
+
       <div className='grid grid-cols-1 sm:grid-cols-2  text-base gap-x-5 gap-y-2'>
         {options.map((el) => (
           <i
